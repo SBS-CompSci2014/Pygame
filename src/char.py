0cc4621c1400af075c4abe14c7952d8b1a86e969
Gateau123 <danielw@santbani.org>
2014-05-09 09:12:06 -0400

    Update char.py

---
+++ src/char.py
@@ -0,0 +1,18 @@
+# Char
+
+
+#make the thing move
+#hashtag
+x=0,0
+movex = 0,0
+
+while True:
+
+def moveLeft():
+  #hashtag
+if event.key == K_LEFT:
+	movex = -1
+
+def moveRight():
+if event.key==K_RIGHT:
+	movex = +1
